09a0755b41a26fc7692aa70f3096d5700c235996
tyrellshawn <ooluk098@uottawa.ca>
2023-03-26 14:23:03 -0400

    Initial commit

---
+++ src/main/java/ca/errandshare/migrate/DataDriverApplication.java
@@ -0,0 +1,13 @@
+package ca.errandshare.migrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataDriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataDriverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/errandshare/migrate/DataDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.errandshare.migrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataDriverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
